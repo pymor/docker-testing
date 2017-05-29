8a39ab2e315721762e5bc04bcc404ca60c89ff9e
Rene Milk <rene.milk@wwu.de>
2017-05-29 11:58:19 +0200

    [base] fix python package versions to not break dolfin

--- base/xenial/fenics/build_and_test.bash
+++ base/xenial/fenics/build_and_test.bash
@@ -3,12 +3,12 @@
 set -e
 set -x
 
-pip install --no-cache-dir ply sympy && \
+pip install --no-cache-dir ply sympy==1.0 && \
     pip install --no-cache-dir matplotlib && \
     pip install --no-cache-dir https://bitbucket.org/mpi4py/mpi4py/downloads/mpi4py-${MPI4PY_VERSION}.tar.gz && \
     pip install --no-cache-dir https://bitbucket.org/petsc/petsc4py/downloads/petsc4py-${PETSC4PY_VERSION}.tar.gz && \
-    pip install --no-cache-dir jupyter && \
-    pip install --no-cache-dir ipython ipykernel
+    pip install --no-cache-dir jupyter==1.0.0 && \
+    pip install --no-cache-dir ipython==5.1.0 ipykernel==4.5.2
 
 FENICS_SRC_DIR=/tmp/src $FENICS_HOME/fenics-build && \
     ldconfig && \
--- base/xenial/install_requirements.bash
+++ base/xenial/install_requirements.bash
@@ -2,7 +2,7 @@
 
 git clone https://github.com/pymor/pymor /tmp/pymor && \
     pip install -U pip && \
-    pip install -U numpy && \
+    pip install numpy==1.11 && \
     pip install -r /tmp/pymor/requirements.txt && \
     pip install -r /tmp/pymor/requirements-travis.txt && \
     pip install -r /tmp/pymor/requirements-optional.txt || echo "Some optional modules failed to install" && \
