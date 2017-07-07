44b2e35398438cf576287fa43c14df6da684b500
Rene Milk <rene.milk@wwu.de>
2017-07-07 10:08:31 +0200

    [base] let errors in optional pip req. fail the build
    this was actually hiding a regression in pyside installing

--- base/xenial/install_requirements.bash
+++ base/xenial/install_requirements.bash
@@ -5,5 +5,5 @@ git clone https://github.com/pymor/pymor /tmp/pymor && \
     pip install numpy==1.11 && \
     pip install -r /tmp/pymor/requirements.txt && \
     pip install -r /tmp/pymor/requirements-travis.txt && \
-    pip install -r /tmp/pymor/requirements-optional.txt || echo "Some optional modules failed to install" && \
+    pip install -r /tmp/pymor/requirements-optional.txt && \
     rm -rf /tmp/pymor
