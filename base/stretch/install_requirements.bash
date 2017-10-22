30cb45f048ba7db92892faf84208dbd3a4fe05fd
René Milk <rene.milk@wwu.de>
2017-10-22 21:27:29 +0200

    there's no petsc mix-in image any more

--- base/stretch/install_requirements.bash
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-git clone https://github.com/pymor/pymor /tmp/pymor && \
-    pip install -U pip && \
-    pip install numpy==1.11 && \
-    pip install -r /tmp/pymor/requirements.txt && \
-    pip install -r /tmp/pymor/requirements-travis.txt && \
-    pip install -r /tmp/pymor/requirements-optional.txt && \
-    rm -rf /tmp/pymor
