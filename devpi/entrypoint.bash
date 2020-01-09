9cbe132d0adfad3aa11e9e3667c2be57e92217db
René Fritze <rene.fritze@wwu.de>
2020-01-09 15:46:56 +0100

    [devpi] fixup init

---
+++ devpi/entrypoint.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+devpi-server --restrict-modify root --start --host 127.0.0.1 --port 3141 --init
+devpi-server --status
+devpi use http://localhost:3141
+devpi login root --password=''
+devpi user -m root password="${DEVPI_PASSWORD}"
+devpi index -y -c public pypi_whitelist='*'
+devpi-server --stop
+devpi-server --status
+
+exec devpi-server --restrict-modify root --host 0.0.0.0 --port 3141
