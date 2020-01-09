5d30299a6700ded790daf90d132164d719bcca39
René Fritze <rene.fritze@wwu.de>
2020-01-09 16:29:35 +0100

    [deploy checks] fix custom pip.conf

--- devpi/entrypoint.bash
+++ devpi/entrypoint.bash
@@ -5,7 +5,7 @@ devpi-server --status
 devpi use http://localhost:3141
 devpi login root --password=''
 devpi user -m root password="${DEVPI_PASSWORD}"
-devpi index -y -c public pypi_whitelist='*'
+devpi index -y -c public bases=root/pypi pypi_whitelist='*'
 devpi-server --stop
 devpi-server --status
 
